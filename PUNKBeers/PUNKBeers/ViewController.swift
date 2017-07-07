1b78946873804f18be43844af25d524162456582
rnas <rnaswaty@gmail.com>
2017-07-06 22:58:04 -0300

    [add] Initial layout files

---
+++ PUNKBeers/PUNKBeers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PUNKBeers
+//
+//  Created by rnas on 06/07/17.
+//  Copyright © 2017 rnas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
